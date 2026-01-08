6ac55fe84da5c594371704770a07ca9eaf700129
gitlab.com/pepa65 <pepa65@passchier.net>
2026-01-08 23:44:14 +0700

    First rerelease

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,44 +1,38 @@
 use aes_gcm::{
-    aead::{Aead, KeyInit},
-    Aes256Gcm, Nonce,
+	Aes256Gcm, Nonce,
+	aead::{Aead, KeyInit},
 };
-use anyhow::{anyhow, Context, Result};
-use rand::{thread_rng, Rng};
+use anyhow::{Context, Result, anyhow};
+use rand::{Rng, rng};
 use sha2::{Digest, Sha256};
 
 const NONCE_SIZE: usize = 12;
 
 pub fn encrypt(plaintext: &[u8], key: Key) -> Result<Vec<u8>> {
-    let nonce_data: [u8; NONCE_SIZE] = thread_rng().gen();
-    let ciphertext = key
-        .cipher
-        .encrypt(Nonce::from_slice(&nonce_data), plaintext)
-        .map_err(|e| anyhow!("encrypt: {e}"))?;
-    let mut output_data = Vec::from(nonce_data);
-    output_data.extend(ciphertext);
-    Ok(output_data)
+	let nonce_data: [u8; NONCE_SIZE] = rng().random();
+	let ciphertext = key.cipher.encrypt(Nonce::from_slice(&nonce_data), plaintext).map_err(|e| anyhow!("encrypt: {e}"))?;
+	let mut output_data = Vec::from(nonce_data);
+	output_data.extend(ciphertext);
+	Ok(output_data)
 }
 
 pub fn decrypt(ciphertext: &[u8], key: Key) -> Result<Vec<u8>> {
-    if ciphertext.len() < NONCE_SIZE {
-        anyhow::bail!("encrypted data too short")
-    }
-    let (nonce_data, ciphertext) = ciphertext.split_at(NONCE_SIZE);
-    let plaintext = key
-        .cipher
-        .decrypt(Nonce::from_slice(nonce_data), ciphertext)
-        .map_err(|e| anyhow!("decrypt: {e}"))?;
-    Ok(plaintext)
+	if ciphertext.len() < NONCE_SIZE {
+		anyhow::bail!("encrypted data too short")
+	}
+	let (nonce_data, ciphertext) = ciphertext.split_at(NONCE_SIZE);
+	let plaintext = key.cipher.decrypt(Nonce::from_slice(nonce_data), ciphertext).map_err(|e| anyhow!("decrypt: {e}"))?;
+	Ok(plaintext)
 }
 
 pub struct Key {
-    cipher: Aes256Gcm,
+	cipher: Aes256Gcm,
 }
 
 impl Key {
-    pub fn new(key: impl AsRef<[u8]>) -> Result<Self> {
-        let key: [u8; 32] = Sha256::digest(key).into();
-        let cipher = Aes256Gcm::new_from_slice(&key).context("key from slice")?;
-        Ok(Self { cipher })
-    }
+	pub fn new(key: impl AsRef<[u8]>) -> Result<Self> {
+		let key: [u8; 32] = Sha256::digest(key).into();
+		let cipher = Aes256Gcm::new_from_slice(&key).context("key from slice")?;
+		Ok(Self { cipher })
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,117 @@
 mod crypto;
 use anyhow::Context;
 use clap::Parser;
+use clap::builder::Styles;
+use clap::builder::styling::{AnsiColor, Effects};
 use console::Key;
 use std::{
-    fs,
-    io::{stderr, stdout, Read, Write},
-    path::PathBuf,
+	fs,
+	io::{Read, Write, stderr, stdout},
+	path::PathBuf,
 };
 
+const STYLE: Styles = Styles::styled()
+	.header(AnsiColor::Green.on_default().effects(Effects::BOLD))
+	.usage(AnsiColor::Green.on_default().effects(Effects::BOLD))
+	.literal(AnsiColor::Cyan.on_default().effects(Effects::BOLD))
+	.placeholder(AnsiColor::Cyan.on_default())
+	.error(AnsiColor::Red.on_default().effects(Effects::BOLD))
+	.valid(AnsiColor::Cyan.on_default().effects(Effects::BOLD))
+	.invalid(AnsiColor::Yellow.on_default().effects(Effects::BOLD));
+
 #[derive(Debug, Parser)]
-#[clap(name = "rhinopuffin")]
-#[clap(about = "A simple cli tool for encrypting and decrypting files with symmetric encryption.")]
-#[clap(author = "https://ariel.ninja")]
-#[clap(version)]
+#[command(version, about, styles = STYLE, help_template(
+	"\
+{before-help}{name} {version} - {about}
+{usage-heading} {usage}
+{all-args}{after-help}
+"
+))]
 struct Args {
-    /// Input file (omit to read from stdin)
-    #[arg()]
-    file: Option<PathBuf>,
-    /// Decrypt (encrypt by default)
-    #[arg(short, long)]
-    decrypt: bool,
-    /// Output file
-    #[arg(short, long)]
-    output: Option<PathBuf>,
-    /// Use a file as encryption/decryption key
-    #[arg(short, long)]
-    key_file: Option<PathBuf>,
-    /// Use encryption/decryption key string instead of prompting
-    #[arg(short, long)]
-    raw_key: Option<String>,
-    /// Remove input file
-    #[arg(short = 'D', long)]
-    delete: bool,
+	/// Input file (omit to read from stdin)
+	#[arg()]
+	file: Option<PathBuf>,
+	/// Decrypt [default: encrypt]
+	#[arg(short, long)]
+	decrypt: bool,
+	/*/// Output file
+	#[arg(short, long)]
+	output: Option<PathBuf>,*/
+	/// Use a file as encryption/decryption key
+	#[arg(short, long)]
+	keyfile: Option<PathBuf>,
+	/// Password as argument (instead of prompting)
+	#[arg(short, long)]
+	password: Option<String>,
+	/// Remove input file
+	#[arg(short, long)]
+	remove: bool,
 }
 
 fn main() -> anyhow::Result<()> {
-    let args = Args::parse();
+	let args = Args::parse();
 
-    // get data
-    let input_data = if let Some(file) = &args.file {
-        fs::read(file).context("read input file")?
-    } else {
-        let mut data = Vec::new();
-        let mut stdin = std::io::stdin().lock();
-        stdin.read_to_end(&mut data).context("read stdin")?;
-        data
-    };
+	// get data
+	let input_data = if let Some(file) = &args.file {
+		fs::read(file).context("read input file")?
+	} else {
+		let mut data = Vec::new();
+		let mut stdin = std::io::stdin().lock();
+		stdin.read_to_end(&mut data).context("read stdin")?;
+		data
+	};
 
-    // get key
-    let key = if let Some(raw_key) = args.raw_key {
-        crypto::Key::new(raw_key)?
-    } else if let Some(key_file) = args.key_file {
-        let key_data = fs::read(key_file).context("read key file")?;
-        crypto::Key::new(key_data)?
-    } else {
-        let raw_key = prompt("Encryption password: ")?;
-        crypto::Key::new(raw_key)?
-    };
+	// get key
+	let key = if let Some(raw_key) = args.password {
+		crypto::Key::new(raw_key)?
+	} else if let Some(key_file) = args.keyfile {
+		let key_data = fs::read(key_file).context("read key file")?;
+		crypto::Key::new(key_data)?
+	} else {
+		let raw_key = prompt("Password: ")?;
+		crypto::Key::new(raw_key)?
+	};
 
-    // perform encryption/decryption
-    let output_data = if args.decrypt {
-        crypto::decrypt(&input_data, key)?
-    } else {
-        crypto::encrypt(&input_data, key)?
-    };
+	// perform encryption/decryption
+	let output_data = if args.decrypt { crypto::decrypt(&input_data, key)? } else { crypto::encrypt(&input_data, key)? };
 
-    // output
-    if let Some(path) = args.output {
-        fs::write(path, &output_data).context("write output file")?;
-    } else {
-        stdout()
-            .write_all(&output_data)
-            .context("write to stdout")?;
-        stdout().flush().context("flush stdout")?;
-    }
+	// output
+	stdout().write_all(&output_data).context("write to stdout")?;
+	stdout().flush().context("flush stdout")?;
 
-    if args.delete {
-        if let Some(file) = &args.file {
-            std::fs::remove_file(file).context("remove input file")?;
-        }
-    }
-    Ok(())
+	if args.remove
+		&& let Some(file) = &args.file
+	{
+		std::fs::remove_file(file).context("remove input file")?;
+	}
+	Ok(())
 }
 
 fn prompt(prompt_text: &str) -> anyhow::Result<String> {
-    let mut input = String::new();
-    eprint!("{prompt_text}");
-    stderr().flush().context("flush stdout")?;
-    loop {
-        let term = console::Term::stderr();
-        let character = term.read_key().context("read key from terminal")?;
-        match character {
-            Key::Enter => {
-                eprintln!();
-                break;
-            }
-            Key::Char(c) => {
-                input.push(c);
-                eprint!("*");
-                stderr().flush().context("flush stdout")?;
-            }
-            Key::Backspace => {
-                if !input.is_empty() {
-                    term.clear_chars(1).context("clear character")?;
-                }
-                input.pop();
-            }
-            _other_key => continue,
-        };
-    }
-    Ok(input)
+	let mut input = String::new();
+	eprint!("{prompt_text}");
+	stderr().flush().context("flush stdout")?;
+	loop {
+		let term = console::Term::stderr();
+		let character = term.read_key().context("read key from terminal")?;
+		match character {
+			Key::Enter => {
+				eprintln!();
+				break;
+			}
+			Key::Char(c) => {
+				input.push(c);
+				eprint!("*");
+				stderr().flush().context("flush stdout")?;
+			}
+			Key::Backspace => {
+				if !input.is_empty() {
+					term.clear_chars(1).context("clear character")?;
+				}
+				input.pop();
+			}
+			_other_key => continue,
+		};
+	}
+	Ok(input)
 }
