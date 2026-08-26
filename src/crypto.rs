18a74815598a5a92e476421cbf1172dac011c017
gitlab.com/pepa65 <pepa65@passchier.net>
2026-08-26 07:22:00 +0700

    Fix dependencies for security

--- src/crypto.rs
+++ src/crypto.rs
@@ -3,25 +3,28 @@ use aes_gcm::{
 	aead::{Aead, KeyInit},
 };
 use anyhow::{Context, Result, anyhow};
-use rand::{Rng, rng};
+use rand::{RngExt, rng};
 use sha2::{Digest, Sha256};
 
 const NONCE_SIZE: usize = 12;
+const TAG_SIZE: usize = 16;
 
 pub fn encrypt(plaintext: &[u8], key: Key) -> Result<Vec<u8>> {
 	let nonce_data: [u8; NONCE_SIZE] = rng().random();
-	let ciphertext = key.cipher.encrypt(Nonce::from_slice(&nonce_data), plaintext).map_err(|e| anyhow!("encrypt: {e}"))?;
+	let nonce = Nonce::try_from(nonce_data.as_slice())?;
+	let ciphertext = key.cipher.encrypt(&nonce, plaintext).map_err(|e| anyhow!("encrypt: {e}"))?;
 	let mut output_data = Vec::from(nonce_data);
 	output_data.extend(ciphertext);
 	Ok(output_data)
 }
 
 pub fn decrypt(ciphertext: &[u8], key: Key) -> Result<Vec<u8>> {
-	if ciphertext.len() < NONCE_SIZE {
+	if ciphertext.len() < NONCE_SIZE + TAG_SIZE {
 		anyhow::bail!("encrypted data too short")
 	}
 	let (nonce_data, ciphertext) = ciphertext.split_at(NONCE_SIZE);
-	let plaintext = key.cipher.decrypt(Nonce::from_slice(nonce_data), ciphertext).map_err(|e| anyhow!("decrypt: {e}"))?;
+	let nonce = Nonce::try_from(nonce_data)?;
+	let plaintext = key.cipher.decrypt(&nonce, ciphertext).map_err(|e| anyhow!("decrypt: {e}"))?;
 	Ok(plaintext)
 }
 
